c94cec7717226ef6ad09ffa311529018c71112f8
antonia.moschin <antonia.moschinn>
2019-03-26 13:20:13 +0100

    Introduction to JavaScript 3-9/10

--- workload/3/workload3.js
+++ workload/3/workload3.js
@@ -1,3 +1,4 @@
+//5/10
 // Opening line
 console.log('It was love at first sight.');
 /*
@@ -9,4 +10,31 @@ console.log('If it didn\'t become jaundice and went away they could discharge hi
 console.log('But this just being short of jaundice all the time confused them.');
 */
 
-testing
+//Aufgabe 4/10
+console.log('JavaScript')
+console.log(2011)
+console.log('Woohoo! I love to code! #codecademy')
+console.log(20.49)
+
+//Aufgabe 5/10
+console.log(27+3.5)
+console.log(2019-1969)
+console.log(65/240)
+console.log(0.2708*100)
+
+//Aufagbe 6/10
+console.log('Hello' + 'World');
+console.log('Hello'+ ' ' + 'World');
+
+//Aufgabe 7/10
+console.log('Teaching the world how to code'.length);
+
+//Aufgabe 8/10
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+//Aufgabe 9/10
+console.log (Math.floor(Number.isInteger(2017)))
