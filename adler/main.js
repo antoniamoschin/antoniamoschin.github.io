2b5dbd3915ab0ca9e307babc7eaac5e38b057e6b
Moschin <csap6999@uibk.ac.at>
2019-03-27 18:33:56 +0100

    Array einfügen in Blicke.js

--- adler/main.js
+++ adler/main.js
@@ -39,54 +39,15 @@ let pin2 = L.marker(
 //Popup zum Pin hängen
 pin2.bindPopup(titel2).openPopup();
 
-const blick1 = {
-    kunde: "Wilder Kaiser",
-    standort: "Gruttenhütte",
-    seehoehe: 1640,
-    lat: 47.55564,
-    lng: 12.31861,
-};
 
-let pin3 = L.marker(
-    [blick1.lat, blick1.lng]
-).addTo(karte);
-pin3.bindPopup(
-    `<h1>Standort ${blick1.standort} </h1>
-    <p> Höhe: ${blick1.seehoehe} m </p>
-    <em> Kunde: ${blick1.kunde}</em>
-    `);
-
-
-    const blick2 = {
-        kunde: "Bergbahn Scheffau",
-        standort: "Brandstadl",
-        seehoehe: 1640,
-        lat: 47.4912,
-        lng: 12.248,
-    };
-    
-    let pin4 = L.marker(
-        [blick2.lat, blick2.lng]
+for (let blick of adlerblicke) {
+    console.log(blick);
+    let blickpin = L.marker(
+        [blick.lat, blick.lng]
     ).addTo(karte);
-    pin4.bindPopup(
-        `<h1>Standort ${blick2.standort} </h1>
-        <p> Höhe: ${blick2.seehoehe} m </p>
-        <em> Kunde: ${blick2.kunde}</em>
-        `);
-
-        const blick3 = {
-            kunde: "Lechtal Tourismus",
-            standort: "Sonnalm Jöchelspitze",
-            seehoehe: 1786,
-            lat: 47.41028,
-            lng: 10.60083,
-        };
-        
-        let pin5 = L.marker(
-            [blick3.lat, blick3.lng]
-        ).addTo(karte);
-        pin5.bindPopup(
-            `<h1>Standort ${blick3.standort} </h1>
-            <p> Höhe: ${blick3.seehoehe} m </p>
-            <em> Kunde: ${blick3.kunde}</em>
-            `);
+    blickpin.bindPopup(
+        `<h1>Standort ${blick.standort}</h1>
+        <p>Höhe: ${blick.seehoehe}</p>
+        <em> Kunde: ${blick.kunde}</em>`
+    )
+}
