5c29da31d3b8dd8e946fbe19fdd96967a2ca513d
antonia.moschin <antonia.moschinn>
2019-03-27 09:17:40 +0100

    Variables 3/10

--- workload/3/variables.js
+++ workload/3/variables.js
@@ -3,3 +3,8 @@ var favoriteFood = 'pizza';
 var numOfSlices = 8;
 console.log(favoriteFood);
 console.log(numOfSlices);
+
+//Aufgabe 3/10
+let changeMe = true;
+changeMe = false;
+console.log(changeMe)
