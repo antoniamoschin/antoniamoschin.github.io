ea7c9ec5f44489fc15563a40cedceaae68f3a49d
Moschin <csap6999@uibk.ac.at>
2019-04-03 16:13:11 +0200

    Basemap 1

--- adler/main.js
+++ adler/main.js
@@ -21,10 +21,17 @@ karte.setView(
 );
 
 //openstreetmap einbauen - s=server, z= zoom, x=laenge, y=breite
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+/*L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
     subdomains: ["a", "b", "c"], 
     attribution: `Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>`
-}).addTo(karte);
+}).addTo(karte);*/
+
+L.tilelayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+    subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"], 
+    attribution: 'Datenquelle: <a href="https://ww.basemap.at"> basemap.at>/a>'
+}).addTo(karte); 
+
+
 
 //Positionmaker 1 setzen
 let pin1 = L.marker(
