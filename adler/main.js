4259c3c98e368642118c27fe22e2bdd56f5b105c
Moschin <csap6999@uibk.ac.at>
2019-04-03 17:53:56 +0200

    plugin hinzufügen

--- adler/main.js
+++ adler/main.js
@@ -21,45 +21,102 @@ karte.setView(
 );
 
 const kartenLayer = {
-        osm: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
-            subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-            attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
-        }), 
-        geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
-            subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-            attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
-    })
-}; 
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: `Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>`
+    }),
+    geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapoverlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapgrau: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmaphidpi: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpg", { //high DPI
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bbmaporthofoto30cm: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpg", { //Orthophoto
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapgelaende: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgelaende/grau/google3857/{z}/{y}/{x}.jpg", { //Gelände
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
 
-    //kartenlayer.osm.addTo(karte)
-    kartenLayer.geolandbasemap.addTo(karte); 
+    bmapoberflaeche: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoberflaeche/grau/google3857/{z}/{y}/{x}.jpg", { //Oberfläche
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    stamen_toner: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"] , 
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.', 
+    }),
+    stamen_terrain: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg", {
+        subdomains:  ["a", "b", "c"] , 
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.', 
+    }),
+    stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
+        subdomains:  ["a", "b", "c"] , 
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.', 
+    }),
+    };
 
 
-        //Positionmaker 1 setzen
-        let pin1 = L.marker(
-            [breite1, laenge1]
-        ).addTo(karte);
+kartenLayer.geolandbasemap.addTo(karte);
 
-        //Popup zum Pin hängen
-        pin1.bindPopup(title1).openPopup();
+//Auswahlmenü hinzuügen: 
+L.control.layers({
+    "Geoland Basemap": kartenLayer.geolandbasemap, 
+    "Geoland Basemap Grau": kartenLayer.bmapgrau, 
+    "OpenStreetMap" : kartenLayer.osm, 
+    "Geoland Basemap Overlay": kartenLayer.bmapoverlay, 
+    "Geoland Basemap High DPI": kartenLayer.bmaphidpi, 
+    "Geoland Basemap Orthofoto": kartenLayer.bbmaporthofoto30cm, 
+    "Geoland Basemap Gelände": kartenLayer.bmapoberflaeche, 
+    "Toner": kartenLayer.stamen_toner, 
+    "Terrain": kartenLayer.stamen_terrain, 
+    "Watercolor": kartenLayer.stamen_watercolor, 
 
-        //Positionmaker 2 setzen
-        let pin2 = L.marker(
-            [breite2, laenge2]
-        ).addTo(karte);
+}).addTo(karte); 
 
-        //Popup zum Pin hängen
-        pin2.bindPopup(titel2).openPopup();
+//kartenLayer.osm.addTo(karte);
+//kartenLayer.geolandbasemap.addTo(karte); 
+//kartenLayer.geolandbasemapoverlay.addTo(karte); 
+//kartenLayer.bmaphidpi.addTo(karte); 
 
 
-        for (let blick of ADLERBLICKE) {
-            console.log(blick);
-            let blickpin = L.marker(
-                [blick.lat, blick.lng]
-            ).addTo(karte);
-            blickpin.bindPopup(
-                `<h1>Standort ${blick.standort}</h1>
+//Positionmaker 1 setzen
+let pin1 = L.marker(
+    [breite1, laenge1]
+).addTo(karte);
+
+//Popup zum Pin hängen
+pin1.bindPopup(title1).openPopup();
+
+//Positionmaker 2 setzen
+let pin2 = L.marker(
+    [breite2, laenge2]
+).addTo(karte);
+
+//Popup zum Pin hängen
+pin2.bindPopup(titel2).openPopup();
+
+
+for (let blick of ADLERBLICKE) {
+    console.log(blick);
+    let blickpin = L.marker(
+        [blick.lat, blick.lng]
+    ).addTo(karte);
+    blickpin.bindPopup(
+        `<h1>Standort ${blick.standort}</h1>
         <p>Höhe: ${blick.seehoehe}</p>
         <em> Kunde: ${blick.kunde}</em>`
-            )
-        }
+    )
+}
