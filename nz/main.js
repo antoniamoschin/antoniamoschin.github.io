cd612acfebeff6782303a5c9e48ddf7572946ecc
Moschin <csap6999@uibk.ac.at>
2019-04-09 10:43:06 +0200

    einfügen karten

--- nz/main.js
+++ nz/main.js
@@ -18,7 +18,29 @@ karte.setView(
 );
 
 //openstreetmap einbauen - s=server, z= zoom, x=laenge, y=breite
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png"). addTo(karte);
+const kartenLayer = {
+osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png"). addTo(karte);
+stamen_toner: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png", {
+    subdomains: ["a", "b", "c"] , 
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.', 
+}),
+stamen_terrain: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg", {
+    subdomains:  ["a", "b", "c"] , 
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.', 
+}),
+stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
+    subdomains:  ["a", "b", "c"] , 
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.', 
+}),
+};
+
+L.control.layers({
+    "Toner": kartenLayer.stamen_toner, 
+    "Terrain": kartenLayer.stamen_terrain, 
+    "Watercolor": kartenLayer.stamen_watercolor, 
+
+}).addTo(map); 
+
 
 //Positionmaker setzen
 let pin = L.marker (
