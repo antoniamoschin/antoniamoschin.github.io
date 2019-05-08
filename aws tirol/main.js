efe68009374c9e6966bb817ed03709b674b169c1
Moschin <csap6999@uibk.ac.at>
2019-05-08 16:26:04 +0200

    AWS konstante Wetterdaten

---
+++ aws tirol/main.js
@@ -0,0 +1,88 @@
+
+
+//Karte initialisieren
+let karte = L.map("map");
+//console.log(karte);
+
+//Kartenlayer hinzufügen
+const kartenLayer = {
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: `Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>`
+    }),
+    geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapoverlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapgrau: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmaphidpi: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpg", { //high DPI
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bbmaporthofoto30cm: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpg", { //Orthophoto
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapgelaende: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgelaende/grau/google3857/{z}/{y}/{x}.jpg", { //Gelände
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+
+    bmapoberflaeche: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoberflaeche/grau/google3857/{z}/{y}/{x}.jpg", { //Oberfläche
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    stamen_toner: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+    }),
+    stamen_terrain: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+    }),
+    stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+    }),
+};
+
+kartenLayer.geolandbasemap.addTo(karte);
+
+//Auswahlmenü hinzuügen: 
+L.control.layers({
+    "Geoland Basemap": kartenLayer.geolandbasemap,
+    "Geoland Basemap Grau": kartenLayer.bmapgrau,
+    "OpenStreetMap": kartenLayer.osm,
+    "Geoland Basemap Overlay": kartenLayer.bmapoverlay,
+    "Geoland Basemap High DPI": kartenLayer.bmaphidpi,
+    "Geoland Basemap Orthofoto": kartenLayer.bbmaporthofoto30cm,
+    "Geoland Basemap Gelände": kartenLayer.bmapoberflaeche,
+    "Toner": kartenLayer.stamen_toner,
+    "Terrain": kartenLayer.stamen_terrain,
+    "Watercolor": kartenLayer.stamen_watercolor,
+
+}).addTo(karte);
+
+// Wikipedia Koordinaten einfügen
+karte.setView(
+    [47.2672222, 11.392778], 15
+);
+
+//console.log(AWS); 
+const awsTirol = L.featureGroup();
+L.geoJson(AWS)
+    .bindPopup(function(layer){
+        console.log("Layer:", layer); 
+        return`Temperatur: ${layer.feature.properties.LT}°C <br>
+        Datum${layer.feature.properties.date} `; 
+    })
+    .addTo(awsTirol);
+awsTirol.addTo(karte);
+karte.fitBounds(awsTirol.getBounds()); 
