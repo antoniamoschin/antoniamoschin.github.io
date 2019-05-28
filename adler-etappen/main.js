3c907b65108f67de6dc1dcdb5de7d4dbdf6d7b42
antonia.moschin <antonia.moschinn>
2019-05-28 17:47:00 +0200

    Tracks anzeigen

--- adler-etappen/main.js
+++ adler-etappen/main.js
@@ -81,3 +81,49 @@ new L.Control.MiniMap(
 ).addTo(karte);
 
 // die Implementierung der Karte startet hier
+
+let pulldown = document.getElementById ("etappenPulldown")
+for (let i=0; i < ETAPPEN.length; i++){
+    //console.log(ETAPPEN[i]); 
+    pulldown.innerHTML += `<option value="${i}">${ETAPPEN[i].titel}</option>`
+}
+//GPX GRuppe erstellen und Menü
+let gpxGruppe = L.featureGroup().addTo(karte); 
+layerControl.addOverlay(gpxGruppe, "GPX-Track"); 
+
+function etappeErzeugen(nummer) {
+    let daten = ETAPPEN[nummer];
+   
+    document.getElementById("daten_titel").innerHTML = daten.titel;  //Anzeigen des ausgewählten ELements aus Dropdown Liste
+    document.getElementById("daten_info").innerHTML = daten.info; 
+   
+    
+
+    //GPX Track laden
+    console.log(daten.gpsid);
+    daten.gpsid =daten.gpsid.replace("A",""); 
+    console.log(daten.gpsid); 
+
+    gpxGruppe.clearLayers();        //nur einen Track anzeigen und der der vorher angezeigt wurde wird nicht mehr angezeigt
+    const gpxTrack = new L.GPX(`gpx/AdlerwegEtappe${daten.gpsid}.gpx`, {
+        async : true, 
+        marker_options : {
+            startIconUrl : 'icons/pin-icon-start.png', 
+            endIconUrl : 'icons/pin-icon-end.png', 
+            shadowUrl : 'icons/pin-shadow.png',
+            iconSize: [32,37]
+        }
+    }). addTo(gpxGruppe); 
+
+    gpxTrack.on("loaded", function(){
+        karte.fitBounds(gpxTrack.getBounds()); 
+    }); 
+}
+etappeErzeugen(0); 
+
+pulldown.onchange = function(evt){
+    let opts = evt.target.options; 
+    console.log(opts[opts.selectedIndex].value); 
+    console.log(opts[opts.selectedIndex].text); 
+    etappeErzeugen(opts[opts.selectedIndex].value);
+}
